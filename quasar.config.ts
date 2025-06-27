82e858f6a1da2de379fcad5ab188267dc5e50d48
Pepotrouille <mechaneka83@gmail.com>
2025-06-27 18:39:49 +0200

    update: path in quasar config

--- quasar.config.ts
+++ quasar.config.ts
@@ -33,6 +33,7 @@ export default defineConfig((/* ctx */) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
     build: {
       target: {
+        publicPath: '/portfolio/',
         browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
         node: 'node20',
       },
